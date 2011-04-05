c3ee4a49efe7e3173de673e739325f2a73d27461
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2011-04-05 01:18:33 -0400

    Added some code for displaying job details when a row is selected. Untested

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -8,3 +8,4 @@
 
 // All strings in app shown to user
 #define kString_GenericErrorMessage					NSLocalizedString(@"An unexpected error has occurred, please try again later\nError code: %@", @"Generic unknown error message")
+#define kString_NoJobDetailsPageFound				NSLocalizedString(@"Job details page not found.", @"Job details page link could not be generated simce job ID was blank")
