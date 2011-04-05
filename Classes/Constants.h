6f014b12113cf30a0f370c011b24c1274e2dee30
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2011-04-04 20:45:38 -0400

    Cleaned up a little bit. Added Constants.h for global constants and strings (user messages)

---
+++ Classes/Constants.h
@@ -0,0 +1,10 @@
+//
+//  Constants.h
+//  Joblet
+//
+//  Created by Sandy Wu on 11-04-03.
+//  Copyright 2011 Sandy Wu. All rights reserved.
+//
+
+// All strings in app shown to user
+#define kString_GenericErrorMessage					NSLocalizedString(@"An unexpected error has occurred, please try again later\nError code: %@", @"Generic unknown error message")
